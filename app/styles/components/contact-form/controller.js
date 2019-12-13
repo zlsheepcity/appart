495f7bbb7f1dd060515c2890562ba85a508e625b
Zinovij Lipinski <zinovi@egamings.com>
2019-12-13 17:09:33 +0200

    form test release

--- app/styles/components/contact-form/controller.js
+++ app/styles/components/contact-form/controller.js
@@ -1,6 +1,6 @@
 function Init__ContactFormReaction (dna) {
-    const defaultSelector = '.form-functional'
-    const Mom = dna.el || document.querySelector(dna.selector || defaultSelector)
+
+    const Mom = dna.el || document.querySelector(dna.selector || '.form-functional')
     if ( !Mom ) return false
 
     const hasStateLoading  = 'has-state-loading'
@@ -10,28 +10,34 @@ function Init__ContactFormReaction (dna) {
     const Dom = {
         Former: Mom,
         Loader: Mom.querySelector('.ajax-loader'),
-        Answer: Mom.querySelector('.form-feedback'),
+        Answer: Mom.querySelector('.wrap-form-feedback'),
         Reform: Mom.querySelector('.js-form-reset'),
+        stLoad: Mom.querySelector('.js-form-loading'),
+        stFeed: Mom.querySelector('.js-form-response'),
         elWpcf: Mom.querySelector('.wpcf7')
     }
 
     const Api = {
 
         setStateLoading: e => {
-            Dom.Former.classList.add(hasStateLoading)
-            Dom.Loader.classList.add(isActive)
+            if (Dom.Former) Dom.Former.classList.add(hasStateLoading)
+            if (Dom.Loader) Dom.Loader.classList.add(isActive)
         },
         remStateLoading: e => {
-            Dom.Former.classList.remove(hasStateLoading)
-            Dom.Loader.classList.remove(isActive)
+            if (Dom.Former) Dom.Former.classList.remove(hasStateLoading)
+            if (Dom.Loader) Dom.Loader.classList.remove(isActive)
         },
         setStateResponse: e => {
-            Dom.Former.classList.add(hasStateResponse)
-            Dom.Answer.classList.add(isActive)
+            if (Dom.Former) Dom.Former.classList.add(hasStateResponse)
+            if (Dom.Answer) Dom.Answer.classList.add(isActive)
         },
         remStateResponse: e => {
-            Dom.Former.classList.remove(hasStateResponse)
-            Dom.Answer.classList.remove(isActive)
+            if (Dom.Former) Dom.Former.classList.remove(hasStateResponse)
+            if (Dom.Answer) Dom.Answer.classList.remove(isActive)
+        },
+        clearResponse: e => {
+            let ResponseToClean = Mom.querySelector('.wrap-validation-response')
+            if (ResponseToClean)  ResponseToClean.innerHTML = ''
         },
         resetState: e => {
             Api.remStateLoading()
@@ -44,6 +50,7 @@ function Init__ContactFormReaction (dna) {
         },
         resetForm: e => {
             Api.resetState()
+            Api.clearResponse()
         },
      // combo
         stateLoading:  e => Api.setState('Loading'),
@@ -51,34 +58,50 @@ function Init__ContactFormReaction (dna) {
     }
 
     const Activate__core_events = ()=> {
-        Dom.Reform.addEventListener('click', Api.resetForm)
+        if (Dom.Reform) Dom.Reform.addEventListener('click', Api.resetForm)
+        if (Dom.stLoad) Dom.stLoad.addEventListener('click', Api.stateLoading)
+        if (Dom.stFeed) Dom.stFeed.addEventListener('click', Api.stateResponse)
     }
 
     const Integration__wpcf7 = ()=> {
+      if ( !Dom.elWpcf ) return false
       const AddEvent = Dom.elWpcf.addEventListener
 
-         // every form submit
-         // AddEvent( 'wpcf7submit',     e=> {} )
-
          // success result
-            AddEvent( 'wpcf7mailsent',   e=> {
-                Api.stateResponse()
-            })
+            AddEvent( 'wpcf7mailsent', Api.stateResponse )
 
          // try again error
-         // AddEvent( 'wpcf7mailfailed', e=> {} )
+         // AddEvent( 'wpcf7mailfailed', e=>{} )
+
+         // every form submit
+         // AddEvent( 'wpcf7submit',     e=>{} )
 
          // validation error
-         // AddEvent( 'wpcf7invalid',    e=> {} )
+         // AddEvent( 'wpcf7invalid',    e=>{} )
 
-            // Helpers:
-            //  const inputs = e.detail.inputs
-            //  inputs[0].name
-            //  inputs[0].value
+         // Fields data inside event functions:
+         //     const inputs = e.detail.inputs
+         //     inputs[0].name
+         //     inputs[0].value
     }
 
- // Run
+ // Initialize
     Activate__core_events()
     Integration__wpcf7()
-    return true
+    return Api
 }
+
+/* --------- wp jquery autorun /*
+
+$(function() {
+    $( '.form-functional' )
+        .each( (i,el) => {Init__ContactFormReaction({el:el})} )
+})
+
+/**//* ----- vanila autorun /*
+
+...
+
+/**/
+
+/** EOF contact form controller */
